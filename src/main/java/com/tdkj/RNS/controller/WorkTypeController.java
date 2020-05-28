291c48e159fed7feed1392d69bea3237b9c70a1e
hxy <houxuyang0801@163.com>
2020-05-28 16:49:15 +0800

    添加进入工种

--- src/main/java/com/tdkj/RNS/controller/WorkTypeController.java
+++ src/main/java/com/tdkj/RNS/controller/WorkTypeController.java
@@ -8,10 +8,14 @@ import com.tdkj.RNS.entity.Worktype;
 import com.tdkj.RNS.service.LogService;
 import com.tdkj.RNS.service.WorkTypeService;
 import com.tdkj.RNS.utils.ShiroUtils;
+import lombok.extern.log4j.Log4j;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -20,6 +24,7 @@ import java.util.List;
  * @version 1.0
  * @date 2020/5/27 16:53
  */
+@Slf4j
 @Controller
 public class WorkTypeController  implements RnsResultCode, RnsResultType {
 
@@ -29,9 +34,27 @@ public class WorkTypeController  implements RnsResultCode, RnsResultType {
     @Autowired
     private LogService logService;
 
+
+    @RequestMapping("/JobSetup")
+    public String JobSetup() {
+        log.info("------------JobSetup");
+        return "/worktype/JobSetup";
+    }
+
+
+    @ResponseBody
     @RequestMapping("/selectJobSetup")
-    public RnsResponse selectJobSetup(Model model) {
+    public RnsResponse selectJobSetup() {
     //前端需要传过来 分页数据 该功能sql 默认为0-10
+        List<Worktype> worktypeList = workTypeService.select();
+        Log log = ShiroUtils.setLog("查看工种");
+        logService.insert(log);
+        return RnsResponse.setResult(HTTP_RNS_CODE_200,FIND_SUCCESS,worktypeList);
+    }
+
+    @RequestMapping("/addJobSetup")
+    public RnsResponse addJobSetup() {
+        //前端需要传过来 分页数据 该功能sql 默认为0-10
         List<Worktype> worktypeList = workTypeService.select();
         Log log = ShiroUtils.setLog("查看工种");
         logService.insert(log);
